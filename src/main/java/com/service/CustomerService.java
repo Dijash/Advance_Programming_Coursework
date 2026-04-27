c7cb5851e5b97342956f6175584dc857bea9d64b
Kushal Khanal <kritikakhanal@Kushal-MacBook-Air.local>
2026-04-27 12:57:58 +0545

    Admin Changes

--- src/main/java/com/service/CustomerService.java
+++ src/main/java/com/service/CustomerService.java
@@ -5,5 +5,9 @@ import com.model.Customer;
 import java.util.List;
 
 public class CustomerService {
+    private CustomerDAO customerDAO = new CustomerDAO();
 
+    public List<Customer> getAllCustomers() { return customerDAO.getAllCustomers(); }
+    public Customer getCustomerById(int id) { return customerDAO.getCustomerById(id); }
+    public boolean deleteCustomer(int id) { return customerDAO.deleteCustomer(id); }
 }
